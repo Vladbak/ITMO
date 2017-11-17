892f3a4a36df438fba772f430cdb0f06047fa987
Vladbak <vladbakshen@mail.ru>
2017-11-17 15:17:22 +0400

    ПИП
    что-то исправил. или нет. хз

--- 2 course/IAD/Lab8/src/lab8/CheckBean.java
+++ 2 course/IAD/Lab8/src/lab8/CheckBean.java
@@ -1,9 +1,62 @@
 package lab8;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 
 @ManagedBean
 public class CheckBean {
+    private double x;
+    private double y;
+    private double r;
+    private boolean result;
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getR() {
+        return r;
+    }
+
+    public void setR(double r) {
+        this.r = r;
+    }
+
+    public void ChangeCommandButtonR(String value){
+        setR(Double.parseDouble(value));
+    }
+
+
+    private boolean CheckCircle(double x, double y, double r)
+    {
+        return ((x>0)&&(y>0)&&(x*x+y*y<(r/2)*(r/2)));
+    }
+    private boolean CheckRect(double x, double y, double r)
+    {
+        return ((x>0)&&(y<0)&&(x<r/2)&&(y>-r));
+    }
+    private boolean CheckTriangle(double x, double y, double r)
+    {
+        return ((x<0)&&(y>0)&&(y<2*x+r));
+    }
+
+
+    public boolean Check(double x, double y, double r)
+    {
+        return (CheckCircle(x,y,r) || CheckRect(x,y,r) || CheckTriangle(x,y,r));
+    }
 	
 	
 	
--- 2 course/IAD/Lab8/src/main/webapp/resources/draw.js
+++ 2 course/IAD/Lab8/src/main/webapp/resources/draw.js
@@ -1,18 +1,4 @@
-document.getElementById("R1").addEventListener('click', function() {
-	draw(1);
-});
-document.getElementById("R15").addEventListener('click', function() {
-	draw(1.5);
-});
-document.getElementById("R2").addEventListener('click', function() {
-	draw(2);
-});
-document.getElementById("R25").addEventListener('click', function() {
-	draw(2.5);
-});
-document.getElementById("R3").addEventListener('click', function() {
-	draw(3);
-});
+document.getElementById("svg").addEventListener("onclick", function(evt){drawPoint(evt);});
 
 function draw(r) {
 
@@ -38,32 +24,13 @@ function draw(r) {
 	d3.select("#svg").append("line").attr("x1", 150).attr("x2", 150).attr("y1",
 			0).attr("y2", 300).style("stroke", "black");
 
-	/*
-	 * d3.select("body").append("svg").attr("width", 50).attr("height", 50)
-	 * .append("circle").attr("cx", 25).attr("cy", 25).attr("r", 25)
-	 * .style("fill", "purple");
-	 */
-	// var svg = d3.select('svg').append('svg');
-	//
-	// svg.append('text').text('click somewhere').attr('x', 50).attr('y', 50);
-	//
-	// var events = [];
-	// svg.on('click', function() {
-	// events.push(d3.event);
-	// svg.enter().append('polygone').attr(
-	// 'points',
-	// "150,150 " + String(150 - 20 * r) + ",150 150,"
-	// + String(150 - 40 * r));
-	// if (events.length > 5)
-	// events.shift();
-	// var circles = svg.selectAll('circle').data(events, function(e) {
-	// return e.timeStamp
-	// }).attr('fill', 'gray');
-	// circles.enter().append('circle').attr('cx', function(d) {
-	// return d.x || d.pageX
-	// }).attr('cy', function(d) {
-	// return d.y || d.pageY
-	// }).attr('fill', 'red').attr('r', 10);
-	// circles.exit().remove();
-	// });
+}
+
+function drawPoint(evt){
+	var x = evt.clientX;
+	var y = evt.clientY;
+
+	d3.select("#svg").append("circle").attr("cx", String(x)).attr("cy", String(y)).attr("r", "3");
+
+
 }
