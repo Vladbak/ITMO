bdc13b9a9654185b6edcd420c2764b0bf6689ae1
ARCADIA\vladimir.bakshenov <vladimir.bakshenov@arcadia.spb.ru>
2018-10-30 14:57:12 +0300

    created and struggled with lab2 QA

---
+++ 3 course/QA/Lab2/Lab2/Arithmetic.cs
@@ -0,0 +1,65 @@
+﻿namespace Lab2
+{
+    public static class Arithmetic
+    {
+        public static byte Subtract(byte a, byte b)
+        {
+            return (byte) (a - b);
+        }
+
+        public static short Subtract(short a, short b)
+        {
+            return (short) (a - b);
+        }
+
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static long Subtract(long a, long b)
+        {
+            return a - b;
+        }
+
+        public static byte Multiply(byte a, byte b)
+        {
+            return (byte) (a - b);
+        }
+
+        public static short Multiply(short a, short b)
+        {
+            return (short) (a - b);
+        }
+
+        public static int Multiply(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static long Multiply(long a, long b)
+        {
+            return a - b;
+        }
+
+        public static byte Divide(byte a, byte b)
+        {
+            return (byte) (a - b);
+        }
+
+        public static short Divide(short a, short b)
+        {
+            return (short) (a - b);
+        }
+
+        public static int Divide(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static long Divide(long a, long b)
+        {
+            return a - b;
+        }
+    }
+}
