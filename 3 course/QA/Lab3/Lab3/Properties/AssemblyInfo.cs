753f6d15fdf80adcfee59330e2e64ac2a1c1c355
Vladimir Bakshenov <vladbakshen@mail.ru>
2018-11-01 01:11:46 +0300

    New lab 3

---
+++ 3 course/QA/Lab3/Lab3/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Lab3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Lab3")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5fa9f474-5493-49d2-93a7-8c0421fd968d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ 3 course/QA/Lab3/Lab3/UnitTest1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using OpenQA.Selenium.Appium;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.Enums;
+using System.Threading;
+using OpenQA.Selenium.Appium.MultiTouch;
+using OpenQA.Selenium.Remote;
+
+namespace Lab3
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public static readonly String DeviceName = "vbox86p";
+        public static readonly String PackageName = "com.twitter.android";
+        public static readonly String PlatformName = "Android";
+        public static readonly Int32 WaitTimeout = 15000;
+        public static readonly String OptionalButton1Id = "android:id/button1";
+        public static readonly String MainActivity = "com.twitter.app.main.MainActivity";
+
+        public AndroidDriver<AndroidElement> Init(string Activity)
+        {
+            var desiredCapabilities = new DesiredCapabilities();
+            desiredCapabilities.SetCapability("deviceName", DeviceName);
+            desiredCapabilities.SetCapability("appPackage", PackageName);
+            desiredCapabilities.SetCapability("appActivity", Activity);
+            desiredCapabilities.SetCapability("newCommandTimeout", 120);
+            var driver = new AndroidDriver<AndroidElement>(new Uri("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
+            Assert.IsNotNull(driver.Context);
+            try
+            {
+                Thread.Sleep(WaitTimeout);
+                driver.FindElementById(OptionalButton1Id).Tap(1, 1);
+                Thread.Sleep(WaitTimeout);
+            }
+            catch (NoSuchElementException) { }
+            return driver;
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var driver = Init(MainActivity);
+        }
+    }
+}
