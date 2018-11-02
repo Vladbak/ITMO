9476c04793d6ac176a20d65f746f914c77dd0b81
Vladimir Bakshenov <vladbakshen@mail.ru>
2018-11-02 15:24:29 +0300

    Lab3 Kate Mobile is working

--- 3 course/QA/Lab3/Lab3/UnitTest1.cs
+++ 3 course/QA/Lab3/Lab3/UnitTest1.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 using OpenQA.Selenium.Appium.Android;
 using OpenQA.Selenium.Remote;
 
@@ -16,14 +18,14 @@ namespace Lab3
         public static readonly Int32 WaitTimeout = 15000;
 
         public static readonly String MainActivity = "com.perm.kate.MainActivity";
-        public static readonly String GotItButton = "//*[@resource-id='com.google.android.gm:id/welcome_tour_got_it]";
-        public static readonly String AddNewEmail = "//*[@resource-id='com.google.android.gm:id/setup_addresses_add_another']";
-        public static readonly String OtherEmailsButton = "//*[@resource-id='com.google.android.gm:id/providers_list']/android.widget.LinearLayout[5]/android.widget.LinearLayout";
-        public static readonly String NewEmailTextArea = "//android.widget.EditText";
-        public static readonly String LoginInput = "//*[@resource-id='com.facebook.katana:id/login_username']";
-        public static readonly String PasswordInput = "//*[@resource-id='com.facebook.katana:id/login_password']";
-        public static readonly String SignInButton = "//*[@resource-id='com.facebook.katana:id/login_password']";
+        public static readonly String LoginInput = "//*[@resource-id='com.perm.kate:id/username']";
+        public static readonly String PasswordInput = "//*[@resource-id='com.perm.kate:id/password']";
+        public static readonly String LoginButton = "//*[@resource-id='com.perm.kate:id/login_btn']";
 
+        public static readonly String MessagesTab = "//*[@resource-id='com.perm.kate:id/action_messages']";
+        public static readonly String TextArea = "//*[@resource-id='com.perm.kate:id/et_new_message']";
+        public static readonly String SendMessageButton = "//*[@resource-id='com.perm.kate:id/btn_new_message']";
+        public static readonly String MyDialog = "//android.view.View/android.widget.ListView/android.widget.LinearLayout/android.widget.ImageView[@content-desc='Profile Владимир Бакшенов']/../android.widget.LinearLayout";
 
         public static readonly String Login= "vladbakshen@mail.ru";
         public static readonly String Password= "1259486370Vk";
@@ -48,17 +50,19 @@ namespace Lab3
             driver = Init(MainActivity);
             driver.FindElementByXPath(LoginInput).SendKeys(Login);
             driver.FindElementByXPath(PasswordInput).SendKeys(Password);
-            driver.FindElementByXPath(SignInName).Click();
+            driver.FindElementByXPath(LoginButton).Click();
         }
 
         [TestMethod]
         public void WriteMessage()
         {
-            driver.FindElementByXPath(Messages).Click();
+            Thread.Sleep(2000);
+            driver.FindElementByXPath(MessagesTab).Click();
+            Thread.Sleep(2000);
             driver.FindElementByXPath(MyDialog).Click();
+            Thread.Sleep(2000);
             driver.FindElementByXPath(TextArea).SendKeys(HelloMessage);
-            driver.FindElementByXPath(SendButton).Click();
+            driver.FindElementByXPath(SendMessageButton).Click();
         }
-
     }
 }
