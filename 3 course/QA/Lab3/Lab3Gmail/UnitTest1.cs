9eb4665bbee917d23ae5e57553e34c7a8a24cfd7
Vladimir Bakshenov <vladbakshen@mail.ru>
2018-11-06 01:33:16 +0300

    added working TestMethod for installing widget

--- 3 course/QA/Lab3/Lab3Gmail/UnitTest1.cs
+++ 3 course/QA/Lab3/Lab3Gmail/UnitTest1.cs
@@ -1,16 +1,16 @@
 ﻿using System;
 using System.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 using OpenQA.Selenium.Appium.Android;
 using OpenQA.Selenium.Remote;
-
 
 namespace Lab3Facebook
 {
     [TestClass]
     public class UnitTest1
     {
-        public static readonly String HelloMessage = "Hello!";
+        public static readonly String Test = "test";
 
         public static readonly String DeviceName = "vbox86p";
         public static readonly String PackageName = "com.facebook.katana";
@@ -23,13 +23,22 @@ namespace Lab3Facebook
         public static readonly String PasswordInput= "//*[@resource-id='com.facebook.katana:id/login_password']";
         public static readonly String LoginButton= "//*[@resource-id='com.facebook.katana:id/login_login']";
 
-        //com.facebook.katana:id/login_username com.facebook.katana:id/login_login
-        public static readonly String NewFBPost= "//*[@resource-id='android:id/list']/android.view.View[2]";
-        public static readonly String NewPostTextArea= "//*[@resource-id='com.facebook.katana:id/composer_status_text']";
-        public static readonly String SendPostButton= "//*[@resource-id='com.facebook.katana:id/action_buttons_wrapper']";
+        public static readonly String RememberPswdButton = "//*[@resource-id='com.facebook.katana:id/dbl_on']";
+        public static readonly String FirstCachedAccount= "//*[@resource-id='com.facebook.katana:id/accounts_on_device_container']/*[1]";
+
+        public static readonly String NewPostLink = "//*[@resource-id='android:id/list']/android.view.View[3]";
+        public static readonly String NewPostTextArea = "//*[@resource-id='com.facebook.katana:id/composer_status_text']";
+        public static readonly String TextStyle = "//*[@resource-id='com.facebook.katana:id/text_style_picker']/*[2]";
+        public static readonly String CreateNewPost = "//*[@resource-id='com.facebook.katana:id/primary_named_button']";
+        public static readonly String PrevWidget= "//*[@resource-id='com.dch_android.widget_facebook:id/widgetBackground']";
+        //public static readonly String SendPostButton= "//*[@resource-id='com.facebook.katana:id/action_buttons_wrapper']";
+        //public static readonly String SendPostButton= "//*[@resource-id='com.facebook.katana:id/action_buttons_wrapper']";
+        //public static readonly String SendPostButton= "//*[@resource-id='com.facebook.katana:id/action_buttons_wrapper']";
+        //public static readonly String SendPostButton= "//*[@resource-id='com.facebook.katana:id/action_buttons_wrapper']";
+        //public static readonly String SendPostButton= "//*[@resource-id='com.facebook.katana:id/action_buttons_wrapper']";
         //android:id/list com.facebook.katana:id/composer_status_text com.facebook.katana:id/action_buttons_wrapper
-//        mCurrentFocus=Window{230d61ad u0 com.facebook.katana/com.facebook.composer.act
-//ivity.ComposerActivity}
+        //        mCurrentFocus=Window{230d61ad u0 com.facebook.katana/com.facebook.composer.act
+        //ivity.ComposerActivity}
 
         public static readonly String Login = "etoktotakoy@mail.ru";
         public static readonly String Password = "rapter00";
@@ -52,7 +61,7 @@ namespace Lab3Facebook
         public void LogIn()
         {
             driver = Init(MainActivity);
-            Thread.Sleep(10000);
+            Thread.Sleep(2000);
             try
             {
                 driver.FindElementByXPath(TapOnProfile).Click();
@@ -60,12 +69,14 @@ namespace Lab3Facebook
             catch (Exception e)
             {
                 driver.FindElementByXPath(LoginInput).SendKeys(Login);
+                Thread.Sleep(500);
                 driver.FindElementByXPath(LoginInput).Tap(1, 1);
                 driver.FindElementByXPath(PasswordInput).SendKeys(Password);
+                Thread.Sleep(500);
                 driver.FindElementByXPath(LoginButton).Click();
-                driver.Tap(1, 1050, 1900, 1);
+                Thread.Sleep(3000);
+                driver.FindElementByXPath(RememberPswdButton).Click();
             }
-
         }
 
         [TestMethod]
@@ -75,13 +86,62 @@ namespace Lab3Facebook
         }
 
         [TestMethod]
+        public void I3dle()
+        {
+
+        }
+
+        [TestMethod]
+        public void Id1le()
+        {
+
+        }
+
+        [TestMethod]
         public void NewPost()
         {
             Thread.Sleep(2000);
-            driver.FindElementByXPath(NewFBPost).Click();
-            Thread.Sleep(2000);
-            driver.FindElementByXPath(NewPostTextArea).SendKeys(HelloMessage);
-            driver.FindElementByXPath(SendPostButton).Click();
+            driver.FindElementByXPath(NewPostLink).Click();
+            Thread.Sleep(1000);
+            driver.FindElementByXPath(NewPostTextArea).SendKeys(Test);
+            Thread.Sleep(1000);
+            driver.FindElementByXPath(TextStyle).Click();
+            Thread.Sleep(1000);
+            driver.FindElementByXPath(CreateNewPost).Click();
+            Thread.Sleep(1000);
+        }
+
+        [TestMethod]
+        public void InstallWidget()
+        {
+            var height = driver.Manage().Window.Size.Height;
+            var width = driver.Manage().Window.Size.Width;
+            driver.CloseApp();
+
+            try
+            {
+                driver.FindElementByXPath("//android.appwidget.AppWidgetHostView/android.widget.RelativeLayout");
+                driver.Swipe(600, 400, 500, 200, 5000);
+                Thread.Sleep(1500);
+            }
+            catch (Exception e) { }
+
+            driver.Tap(1, width / 2, height / 2, 2000);
+            Thread.Sleep(1500);
+            driver.Tap(1, driver.FindElementByXPath("//*[@resource-id='com.android.launcher3:id/widget_button']"), 10);
+            Thread.Sleep(1500);
+            driver.Swipe(width * 3 / 4, height / 2, width / 10, height / 2, 700);
+            Thread.Sleep(1500);
+            driver.Swipe(width * 3 / 4, height / 2, width / 10, height / 2, 700);
+            Thread.Sleep(1500);
+            driver.Swipe(width * 3 / 4, height / 2, width / 10, height / 2, 700);
+            Thread.Sleep(1500);
+            driver.Tap(1, driver.FindElementByXPath("//android.widget.GridLayout/android.widget.LinearLayout[5]"), 3000);
+            Thread.Sleep(1500);
+            driver.Tap(1, driver.FindElementByXPath("//*[@resource-id='android:id/button1']"), 10);
+            Thread.Sleep(600);
+            driver.FindElementById("com.dch_android.widget_facebook:id/refresh_button").Click();
+            Thread.Sleep(4000);
         }
     }
 }
