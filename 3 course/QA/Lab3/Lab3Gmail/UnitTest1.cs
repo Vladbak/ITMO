7bd99e137124f4371500467d1bf2eae076a9243a
Vladimir Bakshenov <vladbakshen@mail.ru>
2018-11-07 03:39:37 +0300

    new tests for facebook , i guess

--- 3 course/QA/Lab3/Lab3Gmail/UnitTest1.cs
+++ 3 course/QA/Lab3/Lab3Gmail/UnitTest1.cs
@@ -40,6 +40,15 @@ namespace Lab3Facebook
         //        mCurrentFocus=Window{230d61ad u0 com.facebook.katana/com.facebook.composer.act
         //ivity.ComposerActivity}
 
+        //public static readonly String SendPostButton= "//*[@resource-id='com.facebook.katana:id/action_buttons_wrapper']";
+        public static readonly String SeeAll = "//*[@text='See All']/..";
+        public static readonly String PagesButton2 = "//android.widget.ListView/com.facebook.fbui.widget.contentview.ContentView[2]";
+        public static readonly String PagesButton = "//*[@text='HELP & SETTINGS']/preceding::*[1]";
+        public static readonly String BookmarkId = "com.facebook.katana:id/bookmarks_tab";
+        //android:id/list com.facebook.katana:id/composer_status_text com.facebook.katana:id/action_buttons_wrapper
+        //        mCurrentFocus=Window{230d61ad u0 com.facebook.katana/com.facebook.composer.act
+        //ivity.ComposerActivity}
+
         public static readonly String Login = "etoktotakoy@mail.ru";
         public static readonly String Password = "rapter00";
 
@@ -84,17 +93,126 @@ namespace Lab3Facebook
         {
 
         }
+        
+        public static readonly String UnsubscribeButton= "//android.widget.ListView/android.view.View";
+        public static readonly String FirstPageMoreButton= "//android.support.v7.widget.RecyclerView/com.facebook.fbui.widget.contentview.ContentView/android.widget.Button";
+        public static readonly String FirstPageTitle= "//android.support.v7.widget.RecyclerView/com.facebook.fbui.widget.contentview.ContentView/android.widget.TextView";
+        public static readonly String EnglishOption= "//android.widget.ListView/android.widget.CheckedTextView[2]";
+        public static readonly String DefaultOption= "//android.widget.ListView/android.widget.CheckedTextView[1]";
+        public static readonly String LanguagesBtn= "//*[@text='HELP & SETTINGS']/following-sibling::*[2]";
+        public static readonly String LastPost = "//android.support.v7.widget.RecyclerView/android.view.View";
+        //android.support.v7.widget.RecyclerView
 
         [TestMethod]
-        public void I3dle()
+        public void ChangeLang()
         {
+            var height = driver.Manage().Window.Size.Height;
+            var width = driver.Manage().Window.Size.Width;
+            driver.FindElementById(BookmarkId).Click();
 
+            Thread.Sleep(700);
+            bool notfound = true;
+            while (notfound)
+            {
+                try
+                {
+                    driver.FindElementByXPath(LanguagesBtn).Click();
+                }
+                catch (Exception e)
+                {
+                    driver.Swipe(width / 2, height * 3 / 5, width / 2, height / 5, 500);
+                    continue;
+                }
+                notfound = false;
+            }
+            Thread.Sleep(700);
+
+            driver.FindElementByXPath(EnglishOption).Click();
+            //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(90);
+
+            //driver.FindElementById(BookmarkId).Click();
+            //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(4);
+
+            //Thread.Sleep(700);
+            //notfound = true;
+            //while (notfound)
+            //{
+            //    try
+            //    {
+            //        var a = driver.FindElementByXPath(LanguagesBtn);
+            //        Assert.AreEqual(a.GetAttribute("content-desc").ToLower(), "Язык".ToLower());
+            //        a.Click();
+            //    }
+            //    catch (Exception e)
+            //    {
+            //        driver.Swipe(width / 2, height * 3 / 5, width / 2, height / 5, 500);
+            //        continue;
+            //    }
+            //    notfound = false;
+            //}
+            //Thread.Sleep(700);
+
+            //driver.FindElementByXPath(DefaultOption).Click();
+            //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            //Thread.Sleep(6000);
         }
 
         [TestMethod]
-        public void Id1le()
+        public void Unsubscribe()
         {
 
+            //com.facebook.katana:id/search_edit_text
+            var height = driver.Manage().Window.Size.Height;
+            var width = driver.Manage().Window.Size.Width;
+            //var searchField = driver.FindElementById("com.facebook.katana:id/search_edit_text");
+            //searchField.SendKeys("google");
+            //driver.PressKeyCode(AndroidKeyCode.Enter);
+
+            driver.FindElementById(BookmarkId).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(4);
+
+            Thread.Sleep(700);
+            bool notfound = true;
+            while(notfound)
+            {
+                try
+                {
+                    driver.FindElementByXPath(PagesButton).Click();
+                }
+                catch (Exception e)
+                {
+                    driver.Swipe(width / 2, height * 3 / 5, width / 2, height / 5, 700);
+                    continue;
+                }
+                notfound = false;
+            }
+            Thread.Sleep(700);
+            driver.FindElementByXPath(PagesButton2).Click();
+            notfound = true;
+            while (notfound)
+            {
+                try
+                {
+                    driver.FindElementByXPath(SeeAll).Click();
+                }
+                catch (Exception e)
+                {
+                    driver.Swipe(width / 2, height * 3 / 5, width / 2, height / 5, 700);
+                    continue;
+                }
+                notfound = false;
+            }
+            Thread.Sleep(1000);
+            var pageTitle = driver.FindElementByXPath(FirstPageTitle).GetAttribute("text");
+            driver.FindElementByXPath(FirstPageMoreButton).Click();
+            driver.FindElementByXPath(UnsubscribeButton).Click();
+            driver.PressKeyCode(AndroidKeyCode.Keycode_ESCAPE);
+            driver.FindElementByXPath(SeeAll).Click();
+            var newPageTitle = driver.FindElementByXPath(FirstPageTitle).GetAttribute("text");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            Assert.AreNotEqual(pageTitle, newPageTitle);
         }
 
         [TestMethod]
@@ -109,6 +227,7 @@ namespace Lab3Facebook
             Thread.Sleep(1000);
             driver.FindElementByXPath(CreateNewPost).Click();
             Thread.Sleep(1000);
+            driver.FindElementByXPath(LastPost);
         }
 
         [TestMethod]
@@ -142,6 +261,15 @@ namespace Lab3Facebook
             Thread.Sleep(600);
             driver.FindElementById("com.dch_android.widget_facebook:id/refresh_button").Click();
             Thread.Sleep(4000);
+
+            try
+            {
+                driver.FindElementByXPath("//android.appwidget.AppWidgetHostView/android.widget.RelativeLayout");
+            }
+            catch(Exception e)
+            {
+                Assert.Fail(e.Message);
+            }
         }
     }
 }
